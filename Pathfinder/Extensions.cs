318b922220b73b8e2189f15bfe36ca18bb2c43e5
Tobiky <andreas.hammarstrand@gmail.com>
2017-10-10 22:53:03 +0200

    Grundläggande A* sökalgoritm
    Inte klar, node system och grundläggande random map gen för att testa finns dock.

---
+++ Pathfinder/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Basic_Pathfinder
+{
+    public static class Extensions
+    {
+        public static Point CombinePoints(this Point a, Point b) =>
+            new Point(a.X + b.X, a.Y + b.Y);
+
+        public static float Pyth(GridNode a, GridNode b) =>
+            Pyth(a.Location, b.Location);
+        public static float Pyth(GridPoint a, GridPoint b) =>
+            Pyth(a.Cordinates, b.Cordinates);
+        public static float Pyth(Point a, Point b) =>
+            (float)Math.Sqrt(
+                Math.Pow(MathHelper.Distance(a.X, b.X), 2) +
+                Math.Pow(MathHelper.Distance(a.Y, b.Y), 2)
+                );
+    }
+}
---
+++ Pathfinder/Game1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Basic_Pathfinder
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D defaultTexture;
+        Color defaultColor;
+        Pathfinder pf;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            Window.AllowUserResizing = false;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            defaultColor = new Color(new Random().Next(0, 256), new Random().Next(0, 256), new Random().Next(0, 256));
+
+            MapData.Obstacles = new LinkedList<Rectangle>();
+
+            var tempRand = new Random();
+            int tempInt = Window.ClientBounds.Bottom / new Random().Next(1, Window.ClientBounds.Bottom / new Random().Next(0,35));
+            for (int i = 0; i < tempInt; i++)
+                MapData.Obstacles.AddLast(new Rectangle(tempRand.Next(0, Window.ClientBounds.Right), tempRand.Next(0, Window.ClientBounds.Bottom), 30, 12));
+
+            pf = new Pathfinder(new Point(Window.ClientBounds.Left, Window.ClientBounds.Bottom), new Point(Window.ClientBounds.Right, Window.ClientBounds.Top));
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            defaultTexture = Content.Load<Texture2D>("whitesquare");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            foreach (var obst in MapData.Obstacles)
+                spriteBatch.Draw(defaultTexture, obst, defaultColor);
+
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
