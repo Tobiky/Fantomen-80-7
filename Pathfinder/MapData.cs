76b4912d019ce4dda31b9631c7cbe5378719f365
Tobiky <andreas.hammarstrand@gmail.com>
2017-10-11 17:43:29 +0200

    Update MapData.cs

--- Pathfinder/MapData.cs
+++ Pathfinder/MapData.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,42 +11,10 @@ namespace Basic_Pathfinder
 {
     public static class MapData
     {
-        static Point start = new Point();
-        static Point goal = new Point();
-        public static Point Start { get => start; }
-        public static Point Goal { get => start; }
+        public static Point Start { get; set;  }
+        public static Point Goal { get; set; }
 
         private static bool IsSetUp { get; set; }
         public static LinkedList<Rectangle> Obstacles { get; set; }
-        public static LinkedList<LinkedList<GridPoint>> GridSystem
-        {
-            get
-            {
-                if (IsSetUp)
-                    return GridSystem;
-                else
-                    throw new Exception("GridSystem has not been initialized!");
-            }
-            private set => GridSystem = value;
-        }
-        public static Task SetMap(GameWindow gw, params Rectangle[] obstacles)
-        {
-            var tsc = new TaskCompletionSource<LinkedList<LinkedList<GridPoint>>>();
-            IsSetUp = true;
-            GridSystem = new LinkedList<LinkedList<GridPoint>>();
-            Parallel.For(0, gw.ClientBounds.Bottom, (y) =>
-            {
-                var tGPL = new LinkedList<GridPoint>();
-                GridSystem.AddLast(tGPL);
-                Parallel.For(0, gw.ClientBounds.Right, (x) =>
-                {
-                    var tP = new Point(x, y);
-                    bool t = obstacles.Any((obs) => obs.Contains(tP));
-                    tGPL.AddLast(new GridPoint(tP, t));
-                });
-            });
-            tsc.SetResult(GridSystem);
-            return tsc.Task;
-        }
     }
 }
